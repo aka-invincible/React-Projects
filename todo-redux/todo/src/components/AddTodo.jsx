c8ec5dbb2dec2d2ad000bf1416a2958b336252bf
akaInvincible <akashagarwal1321@gmail.com>
2023-09-19 04:34:26 +0530

    Redux Todo with added styles

--- todo-redux/todo/src/components/AddTodo.jsx
+++ todo-redux/todo/src/components/AddTodo.jsx
@@ -13,9 +13,20 @@ const AddTodo = () => {
     }
   return (
     <div>
-      <form onSubmit={addTodoHandler}>
-        <input type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Enter a Todo...' />
-        <button type='submit'>Add Todo</button>
+      <form onSubmit={addTodoHandler} className="mb-4 my-10">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter a Todo..."
+          className="border border-gray-400 rounded-lg px-4 py-2 w-64 focus:outline-none focus:border-blue-500"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg ml-2"
+        >
+          Add Todo
+        </button>
       </form>
     </div>
   )
--- todo-redux/todo/src/components/Todo.jsx
+++ todo-redux/todo/src/components/Todo.jsx
@@ -8,11 +8,20 @@ const Todo = () => {
   return (
     <>
       <div>
-        Todos
+        <span className="font-bold text-3xl">Todos</span>
       </div>
-      {todos.map(todo =>
-        <p key={todo.id}>{todo.text} <button onClick={(id) => dispatch(removeTodo(todo.id))}>delete</button></p>
-        )}
+      {todos.map((todo) => (
+        <p key={todo.id} className="flex mx-10 px-7 items-center justify-between border border-gray-300 rounded-lg p-2 mb-2">
+          <span className="text-gray-800"><i>{todo.text}</i></span>
+          <button
+            onClick={() => dispatch(removeTodo(todo.id))}
+            className="bg-red-500 hover:bg-red-600 text-white font-semibold px-2 py-1 rounded-md focus:outline-none"
+          >
+            Delete
+          </button>
+        </p>
+      ))}
+
     </>
   )
 }
