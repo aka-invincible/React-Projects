134b383c57958eef94e89af14355367671bcba2a
akaInvincible <akashagarwal1321@gmail.com>
2023-10-05 03:23:59 +0530

    Auth Service using appwrite

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import conf from "../conf/conf";
+import { client, account, ID } from 'appwrite';
+
+export class AuthService {
+    client = new client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteURL)
+            .setProject(conf.appwriteProjectId);
+
+        this.account = new account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.account.login(email, password);
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async login(email, password) {
+        try {
+            await this.account.createEmailSession(email, password);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.error(error);
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
